a02ac8b0fb4cf3fe64d2e65ac06e814f7d16e40f
Kyle Qi <kyle.et.qi@gmail.com>
2026-01-21 20:01:59 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <header className="App-header">
+      <h1>Welcome to Kyle Qi's Website</h1>
+      <p>This is a temporary landing page.</p>
+    </header>
+  )
+}
+
+export default App
+
+

c26a8c4295eb5ddffe4592fa6bab90d872252533
Kyle Qi <kyle.et.qi@gmail.com>
2026-01-21 20:16:41 -0800

    Add contact information

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import './App.css'
 function App() {
   return (
     <header className="App-header">
-      <h1>Welcome to Kyle Qi's Website</h1>
-      <p>This is a temporary landing page.</p>
+      <h1>Kyle Qi</h1>
+      <p>Mechatronics @ uWaterloo</p>
+      <p>This is a temporary landing page</p>
+      <p className="experience"><strong>Experience:</strong></p>
+      <p>Avionics @ Zipline</p>
+      <p>Prev. AI Hardware @ Tesla</p>
+      <p className="contacts"><strong>Contacts:</strong></p>
+      <p><a href="https://linkedin.com/in/kyleqi-">linkedin.com/in/kyleqi-</a></p>
+      <p><a href="https://github.com/kyle-qi">github.com/kyle-qi</a></p>
+      <p><a href="mailto:contact@kyleqi.com">contact@kyleqi.com</a></p>
     </header>
   )
 }
